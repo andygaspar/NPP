cef49599db529596fee0a5e023d4a01d5969351d
andrea <andygaspar@libero.it>
2022-10-19 12:39:03 +0200

    sistemato grafo

--- Instance/instance.py
+++ Instance/instance.py
@@ -33,16 +33,16 @@ class Instance:
         self.npp.add_nodes_from([(u, {'color': 'g'}) for u in self.locations])
         self.npp.add_nodes_from([(t, {'color': 'r'}) for t in self.tolls])
 
-        for arc in [(i, j) for i in self.locations for j in self.locations if i < j]:
+        edges = [(i, j) for i in self.locations for j in self.locations if i < j]
+        for arc in edges:
             self.npp.add_edge(*arc, color='g', weight=np.random.uniform(*self.cr_locations))
 
         for arc in [p for p in self.toll_paths]:
             self.npp.add_edge(*arc, color='r', weight=np.random.uniform(*self.cr_transfer))
 
-        for arc in [(i, j) for i in self.locations for j in self.tolls]:
+        transfers = [(i, j) for i in self.locations for j in self.tolls]
+        for arc in transfers:
             self.npp.add_edge(*arc, color='b', weight=np.random.uniform(*self.cr_transfer))
-
-
 
         origin_destination = list(combinations(self.locations, r=2))
 
@@ -53,7 +53,18 @@ class Instance:
 
         self.N_p = {p: max([k.M_p[p] for k in self.commodities]) for p in self.toll_paths}
 
+        for comm in self.commodities:
+            if (comm.origin, comm.destination) in edges:
+                edges.remove((comm.origin, comm.destination))
+            for toll in self.tolls:
+                if (comm.origin, toll) in transfers:
+                    transfers.remove((comm.origin, toll))
+                if (comm.destination, toll) in transfers:
+                    transfers.remove((comm.destination, toll))
 
+
+
+        self.npp.remove_edges_from(edges + transfers)
 
     def show(self):
 
@@ -61,3 +72,11 @@ class Instance:
                 edge_color=[self.npp[u][v]['color'] for u, v in self.npp.edges],
                 with_labels=True, font_size=7)
         plt.show()
+
+    def save_problem(self):
+        # comm: Commodity
+        # commodities_tax_free = [comm.cost_free for comm in self.commodities]
+        # transfer_costs = np.array([[self.npp[comm.origin][tall]['weight'] for tall in self.toll]
+        #                            for comm in self.commodities])
+        # upper_bounds = np.max(transfer_costs, axis=0)
+        print("ll")
--- test.py
+++ test.py
@@ -4,12 +4,8 @@ import time
 import numpy as np
 
 from Instance.instance import Instance
-from PSO.swarm import Swarm
 from Solver.global_ import GlobalSolver
-from Solver.lower import LowerSolver
-from Solver.lower_aggregated import LowerSolverAggregated
 from Solver.pso_solver import PsoSolver
-from Solver.pso_solver_parallel import PsoSolverParallel
 
 """
 
@@ -19,18 +15,18 @@ IMPLEMENTARE BOUND PARTICLE
 """
 os.system("PSO/install.sh")
 
-# n_locations = 10
-# n_commodities = 10
-# n_tolls = 3
-
 n_locations = 10
-n_commodities = 15
-n_tolls = 8
+n_commodities = 10
+n_tolls = 3
+
+# n_locations = 10
+# n_commodities = 15
+# n_tolls = 8
 
 
 
 npp = Instance(n_locations=n_locations, n_tolls=n_tolls, n_commodities=n_commodities, seeds=True)
-
+npp.save_problem()
 npp.show()
 
 t = time.time()
@@ -48,7 +44,7 @@ path_costs = np.random.uniform(size=npp.n_paths*n_particles)
 init_norm_costs = np.random.uniform(size=npp.n_paths*n_particles)
 
 
-s = PsoSolverParallel(npp, init_norm_costs, path_costs, n_particles, n_tolls, n_iterations)
+s = PsoSolver(npp, init_norm_costs, path_costs, n_particles, n_tolls, n_iterations)
 t = time.time()
 s.run()
 t = time.time() - t
