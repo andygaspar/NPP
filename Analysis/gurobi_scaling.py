c6f3ea69ca60fdd64c2dc09920be9f45a9b153d3
fcamerot <fcamerot@gmail.com>
2022-11-29 11:05:00 +0100

    done gurobi scaling test

--- Analysis/gurobi_scaling.py
+++ Analysis/gurobi_scaling.py
@@ -6,9 +6,9 @@ from pathlib import Path
 from Instance.instance import Instance
 from Solver.global_ import GlobalSolver
 
-loc_size = [7, 20, 56]
-comm_size = [8, 20, 56, 90]
-toll_size = [8, 20, 56, 90]
+loc_size = [7, 20]#, 56]
+comm_size = [8, 20, 56]#, 90]
+toll_size = [8, 20, 56]#, 90]
 
 filename = 'gurobi_benchmark_times.txt'
 Path(filename).touch()
