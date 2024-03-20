154a8db72318c4ff964591b7d5a5cb2db384e39d
andrea <andygaspar@libero.it>
2024-03-20 05:52:05 +0100

    refactor completed

--- PSO/swarm.py
+++ PSO/swarm.py
@@ -19,13 +19,13 @@ class SwarmNew:
 
         self.lib.Swarm_.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_int),
                                     ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double),
+                                    ctypes.POINTER(ctypes.c_double),
                                     ctypes.c_short, ctypes.c_short, ctypes.c_short, ctypes.c_int,
                                     ctypes.c_int, ctypes.c_short]
         self.lib.Swarm_.restype = ctypes.c_void_p
 
         self.lib.run_.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double),
-                                  ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.c_bool,
-                                  ctypes.c_bool]
+                                  ctypes.c_bool, ctypes.c_bool]
 
         self.lib.get_best_val_.argtypes = [ctypes.c_void_p]
         self.lib.get_best_val_.restype = ctypes.c_double
@@ -42,11 +42,12 @@ class SwarmNew:
         self.lib.get_v_stds_.argtypes = [ctypes.c_void_p]
 
         n_users = np.array(n_users, dtype=np.intc)
-
+        lower_bounds = np.zeros_like(upper_bounds)
         self.swarm = self.lib.Swarm_(commodities_tax_free.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                                      n_users.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
                                      transfer_costs.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                                      upper_bounds.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                                     lower_bounds.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                                      ctypes.c_short(n_commodities), ctypes.c_short(n_toll_paths),
                                      ctypes.c_short(n_particles), ctypes.c_int(n_iterations),
                                      ctypes.c_int(no_update_lim),
@@ -55,13 +56,11 @@ class SwarmNew:
     def get_best(self):
         return self.lib.get_best_(ctypes.c_void_p(self.swarm)), self.lib.get_best_val_(ctypes.c_void_p(self.swarm))
 
-    def run(self, init_positions, init_velocity, lb, ub, stats=False, verbose=False):
+    def run(self, init_positions, init_velocity, stats=False, verbose=False):
         self.stats = stats
         self.lib.run_(ctypes.c_void_p(self.swarm),
                       init_positions.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                       init_velocity.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                      lb.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                      ub.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                       ctypes.c_bool(stats),
                       ctypes.c_bool(verbose))
         return True
