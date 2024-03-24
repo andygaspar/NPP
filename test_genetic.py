0abceeedf86422ab01f4c25e64b6982eac7338b4
andrea <andygaspar@libero.it>
2024-03-24 11:59:36 +0100

    pre push

--- test_genetic.py
+++ test_genetic.py
@@ -23,12 +23,12 @@ n_iterations = 1000
 n_particles = 20_000
 no_update_lim = 1000
 
-TIME_LIMIT = 30
+TIME_LIMIT = 120
 VERBOSE = False
 row = 0
 
-n_commodities = 96
-n_paths = 96
+n_commodities = 20
+n_paths = 20
 run = 0
 
 print(n_commodities, n_paths, run)
@@ -42,37 +42,38 @@ print("target val", solver.obj)
 
 # solver.print_model()
 
-POPULATION = 1024
+POPULATION = 128
 PARTICLES = 32
 PSO_ITERATIONS = 1000
 ADDITIONAL_PARTICLES = 8
-N_THREADS = None
+MUTATION_RATE = 0.02
+N_THREADS = 1
 
 t = time.time()
 pso = PsoSolverNew(npp, POPULATION, 0, no_update_lim)
 initial_position = pso.random_init()
 npp.compute_solution_value(initial_position[0])
-# 7506.375328431998
+# 7568.098313182916
 particles = np.copy(initial_position)
 
-genetic = Genetic(population_size=initial_position.shape[0], pso_population=ADDITIONAL_PARTICLES, npp=npp,
+genetic = Genetic(population_size=initial_position.shape[0], pso_population=ADDITIONAL_PARTICLES, npp=npp, mutation_rate=MUTATION_RATE,
                   fitness_fun=npp.compute_solution_value, offspring_rate=0.5, n_threads=N_THREADS)
 genetic.parallel_generation(particles)
-for i in range(2000):
+for i in range(10_000):
 
     if i % 100 == 0 and i > 0:
         pso = PsoSolverNew(npp, PARTICLES, n_iterations=PSO_ITERATIONS, no_update_lim=no_update_lim)
-        pso.run(genetic.get_pop_sample(PARTICLES), verbose=True)
+        pso.run(genetic.get_pop_sample(PARTICLES), verbose=False)
         new_particles = pso.get_best_n_particles(ADDITIONAL_PARTICLES)
         genetic.parallel_generation(pso_particles=new_particles)
     else:
         genetic.parallel_generation()
-    if i % 10 == 0 and i > 0:
+    if i % 150 == 0 and i > 0:
         # print(genetic.best_val, np.std(np.std(genetic.population, axis=0)))
-        print(genetic.best_val)
+        print(i, genetic.best_val)
 
-pso = PsoSolverNew(npp, 12, n_iterations=PSO_ITERATIONS, no_update_lim=no_update_lim)
-pso.run(genetic.get_pop_sample(128), verbose=True)
+pso = PsoSolverNew(npp, PARTICLES, n_iterations=PSO_ITERATIONS, no_update_lim=no_update_lim)
+pso.run(genetic.population[:128], verbose=True)
 
 PARTICLES = 10000
 pso = PsoSolverNew(npp, genetic.pop_size, 1000, no_update_lim)
