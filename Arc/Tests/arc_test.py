2a6325fd80bb7693a5b3efd57316dc16d4b847cc
andrea <andrea.gasparin.87@gmail.com>
2024-04-20 09:26:16 +0200

    pre pulling

--- Arc/Tests/arc_test.py
+++ Arc/Tests/arc_test.py
@@ -30,28 +30,29 @@ n_commodities = [40, 50, 60]
 graphs = [GridInstance, DelaunayInstance]
 
 # instance = DelaunayInstance(n_locations, n_arcs, dim_grid, toll_proportion[0], n_commodities[0])
-# instance = GridInstance(n_locations, n_arcs, dim_grid, toll_proportion[2], n_commodities[2])
+instance = GridInstance(n_locations, n_arcs, dim_grid, toll_proportion[2], n_commodities[2])
 
 # instance.show()
-columns = ['run', 'graphs', 'toll_pr', 'n_com', 'obj', 'time', 'gap', 'status']
-row = 0
-df = pd.DataFrame(columns=columns)
-for graph in graphs:
-    for n_c in n_commodities:
-        for n_t in toll_proportion:
-            for run in range(10):
-                print("\nProblem",  n_c, n_t, run)
-                random.seed(run)
-                np.random.seed(run)
-                instance = graph(n_locations, n_arcs, dim_grid, n_t, n_c, seed=run)
+# columns = ['run', 'graphs', 'toll_pr', 'n_com', 'obj', 'time', 'gap', 'status']
+# row = 0
+# df = pd.DataFrame(columns=columns)
+# for graph in graphs:
+#     for n_c in n_commodities:
+#         for n_t in toll_proportion:
+#             for run in range(10):
+#                 print("\nProblem",  n_c, n_t, run)
+#                 random.seed(run)
+#                 np.random.seed(run)
+#                 instance = graph(n_locations, n_arcs, dim_grid, n_t, n_c, seed=run)
+#
+#                 solver = ArcSolver(instance=instance, symmetric_costs=False)
+#                 solver.solve(time_limit=3600, verbose=True)  # int(pso.time))
+#                 df.loc[row] = [run, instance.name, n_t, n_c,solver.obj, solver.time, solver.m.MIPGap, solver.m.status]
+#                 row += 1
 
-                solver = ArcSolver(instance=instance, symmetric_costs=False)
-                solver.solve(time_limit=3600, verbose=True)  # int(pso.time))
-                df.loc[row] = [run, instance.name, n_t, n_c,solver.obj, solver.time, solver.m.MIPGap, solver.m.status]
-                row += 1
-
-df.to_csv('arc_test_solver.csv')
-# solver.obj = 3966.
+# df.to_csv('arc_test_solver.csv')
+solver = ArcSolver(instance=instance, symmetric_costs=False)
+solver.obj = 3966.
 # print(solver.time)
 
 # adj, prices = solver.get_adj_solution()
@@ -60,8 +61,8 @@ df.to_csv('arc_test_solver.csv')
 
 # print(solver.get_tolls())
 #
-# g = GeneticArc(64, instance, offspring_rate=0.2)
-# g.run_cpp_h(100, verbose=True, n_threads=16, seed=0)
+g = GeneticArc(64, instance, offspring_rate=0.2)
+g.run_cpp_h(100, verbose=True, n_threads=16, seed=0)
 #
 # g.run_cpp(0, verbose=True, n_threads=16, seed=0)
 #
